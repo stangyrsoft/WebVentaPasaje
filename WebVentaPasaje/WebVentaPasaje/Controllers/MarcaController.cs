ca6665d9df00002471bd22216c670c461d3f0e7c
Stalin Ynga <stalinyngarojas@outlook.com>
2024-09-12 16:21:54 -0500

    Se crea el controller y el view del modelo Marca

---
+++ WebVentaPasaje/WebVentaPasaje/Controllers/MarcaController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using WebVentaPasaje.Models;
+
+namespace WebVentaPasaje.Controllers
+{
+    public class MarcaController : Controller
+    {
+        // GET: Marca
+        public ActionResult Index()
+        {
+            List<MarcaModel> listMarcas = null;
+            using (var bdPasaje = new BDPasajeEntities())
+            {
+                //Expresion lambda
+                //listMarcas = bdPasaje.Marcas.Select(x => new MarcaModel
+                //{
+                //    Id = x.IIDMARCA,
+                //    Nombre = x.NOMBRE,
+                //    Descripcion = x.DESCRIPCION
+                //}).ToList();
+
+                //Expresion regular
+                listMarcas = (from marca in bdPasaje.Marcas
+                              select new MarcaModel
+                              {
+                                  Id = marca.IIDMARCA,
+                                  Nombre = marca.NOMBRE,
+                                  Descripcion = marca.DESCRIPCION
+                              }).ToList();
+            }
+            return View(listMarcas);
+        }
+    }
+}
---
+++ WebVentaPasaje/WebVentaPasaje/Models/MarcaModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebVentaPasaje.Models
+{
+    public class MarcaModel
+    {
+        [Display(Name = "Codigo Marca")]
+        public int Id { get; set; }
+        [Display(Name = "Nombre Marca")]
+        public string Nombre { get; set; }
+        [Display(Name = "Descripcion Marca")]
+        public string Descripcion { get; set; }
+        public int Bhabilitado { get; set; }
+    }
+}
